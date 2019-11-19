7e707e1ac5f05c78f47fdf1a838b4d00bbef5c83
Augusto Cordero <augusto100295@gmail.com>
2019-11-19 14:20:38 +0100

    set footer in detail

---
+++ E-Commerce WBI/E-Commerce WBI/App/Product/ViewController/ProductDetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ProductDetailViewController.swift
+//  E-Commerce WBI
+//
+//  Created by Augusto C.P. on 11/19/19.
+//  Copyright © 2019 Augusto C.P. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        
+        scrollView.contentInsetAdjustmentBehavior = .never
+       
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.barTintColor = UIColor.clear
+        self.navigationController?.navigationBar.backgroundColor = UIColor.clear
+        
+        let backButton = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+        navigationItem.backBarButtonItem = backButton
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.navigationBar.setBackgroundImage(nil, for: .default)
+        self.navigationController?.navigationBar.shadowImage = nil
+        self.navigationController?.navigationBar.isTranslucent = true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ E-Commerce WBI/E-Commerce WBI/Util/Components/EcoButton.swift
@@ -0,0 +1,18 @@
+//
+//  EcoButton.swift
+//  E-Commerce WBI
+//
+//  Created by Augusto C.P. on 11/19/19.
+//  Copyright © 2019 Augusto C.P. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EcoButton : UIButton {
+    
+    override func draw(_ rect: CGRect) {
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 4
+    }
+}
